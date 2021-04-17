b7f627fbe896d792c5aa603762dce06c06227635
Justin Stanley <jtst@iastate.edu>
2021-04-17 12:16:16 -0500

    add data dir init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate dirs;
+
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let data_dir = dirs::data_dir().unwrap().join("kami");
+
+    // Setup data directories
+    fs::create_dir_all(data_dir).expect("Failed to initialize data directory");
 }
