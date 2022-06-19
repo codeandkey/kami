883aeaf51af2b9679adc3a5b216ee3779c2a36db
Justin Stanley <jtst@iastate.edu>
2022-06-19 16:30:14 -0500

    remove alpha cutoff

--- src/selfplay.h
+++ src/selfplay.h
@@ -24,7 +24,7 @@ namespace kami {
 // MCTS alpha parameter curve
 constexpr float ALPHA_INITIAL = 1.0f;
 constexpr float ALPHA_DECAY = 0.95f;
-constexpr int ALPHA_CUTOFF = 30;
+constexpr int ALPHA_CUTOFF = 1000;
 constexpr float ALPHA_FINAL = 0.0f;
 
 // Path to model
